a1aa4dab51ef5cb4184ed69412e5e58147c1d6eb
Quinn Sissler <QSissler@gmail.com>
2023-02-08 19:44:39 -0500

    commit tree d5d8148bacc4d747c1112d86954a0e3b30ab8975

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+  #    CLICKBAIT_PATTERNS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top \d/i,
+  #   /Guess/i
+  # ]
+
+  # def clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+
+  def clickbait?
+    # unless title.include? "Won't Believe" || "Secret" || "Top[number]" || "Guess"
+    #     errors.add(:title, "requires clickbait-y title")
+    # end
+    unless title != "Won't Believe" || "Secret" || "Top[number]" || "Guess" || "True Facts"
+        errors.add(:title, "requires clickbait-y title")
+    end
+end
+
+
 end
